ea7ce4866fc610939c0dbf8cee699bcd859b1954
thefoxy41 <thefoxy41@gmail.com>
2019-08-08 19:12:41 +0200

    added quotes to server name / updated exception caught

--- OnlineCounter/src/fr/thefoxy41/onlinecounter/commons/Counter.java
+++ OnlineCounter/src/fr/thefoxy41/onlinecounter/commons/Counter.java
@@ -2,17 +2,18 @@ package fr.thefoxy41.onlinecounter.commons;
 
 import fr.thefoxy41.onlinecounter.commons.database.objects.Query;
 import fr.thefoxy41.onlinecounter.commons.database.exceptions.DatabaseConnectionException;
-import fr.thefoxy41.onlinecounter.commons.database.exceptions.DatabaseQueryException;
+
+import java.sql.SQLException;
 
 public class Counter {
     public static void update(int players) {
         String serverName = GlobalConfiguration.MAIN.getServerName();
         try {
             Query query = new Query();
-            query.insertOrUpdate("(name, value) VALUES(" + serverName + ", " + players + ") ON DUPLICATE KEY UPDATE name=" + serverName + ", value=" + players);
+            query.insertOrUpdate("(name, value) VALUES('" + serverName + "', " + players + ") ON DUPLICATE KEY UPDATE name='" + serverName + "', value=" + players);
 
             GlobalConfiguration.MAIN.log("Updated online count for " + serverName + ": " + players + " players online");
-        } catch (DatabaseConnectionException | DatabaseQueryException e) {
+        } catch (DatabaseConnectionException | SQLException e) {
             e.printStackTrace();
         }
     }
